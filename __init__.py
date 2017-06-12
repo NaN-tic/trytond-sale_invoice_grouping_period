3be6da068d2f86eb8b495172b037bb3256f0d879
Raimon Esteve <raimon@nan-tic.com>
2017-06-12 16:16:43 +0200

    Register class from .

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .party import *
-from .sale import *
-from .invoice import *
+from . import party
+from . import sale
+from . import invoice
 
 
 def register():
     Pool.register(
-        Party,
-        Sale,
-        Invoice,
+        party.Party,
+        sale.Sale,
+        invoice.Invoice,
         module='sale_invoice_grouping_period', type_='model')
