678bacd550904e4e492806033edaf7b63f7624e9
Raimon Esteve <raimon@nan-tic.com>
2017-06-13 16:30:15 +0200

    Merge from 3.4 branch

--- sale.py
+++ sale.py
@@ -1,8 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from dateutil.relativedelta import relativedelta
 import datetime
-import calendar
+from dateutil.relativedelta import relativedelta
 from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
 
@@ -10,67 +9,75 @@ __all__ = ['Sale']
 
 
 class Sale:
-    __name__ = 'sale.sale'
     __metaclass__ = PoolMeta
+    __name__ = 'sale.sale'
 
     def _get_grouped_invoice_order(self):
         res = super(Sale, self)._get_grouped_invoice_order()
 
-        if not res:
+        if self.invoice_grouping_method == 'period':
             return [('invoice_date', 'DESC')]
         return res
 
+    def _get_grouped_invoice_date(self):
+        date = None
+        if self.invoice_method == 'shipment':
+            for line in self.lines:
+                for iline in line.get_invoice_line():
+                    for move in iline.stock_moves:
+                        date = move.effective_date
+                        break
+        if date is None:
+            date = self.sale_date
+        return date
+
     def _get_grouped_invoice_domain(self, invoice):
         invoice_domain = super(Sale, self)._get_grouped_invoice_domain(invoice)
-        minimum_date = self._get_minimum_invoice_date()
-        if minimum_date:
-            invoice_domain.append(
-                ('sale_date', '>=', minimum_date)
-                )
-        maximum_date = self._get_maximum_invoice_date()
-        if maximum_date:
-            invoice_domain.append(
-                ('sale_date', '<=', maximum_date)
-                )
+        period = self.party.sale_invoice_grouping_period
+        if self.invoice_grouping_method == 'standard' and period:
+            date = self._get_grouped_invoice_date()
+            start, end = self._get_invoice_dates(date,
+                self.party.sale_invoice_grouping_period)
+            invoice_domain += [
+                ('start_date', '=', start),
+                ('end_date', '=', end),
+                ]
         return invoice_domain
 
-    def _get_minimum_invoice_date(self):
-        period = self.party.sale_invoice_grouping_period
-        if not period:
-            return
+    @staticmethod
+    def _get_invoice_dates(date, period):
         if period == 'monthly':
-            interval_date = relativedelta(months=1)
-            min_date = datetime.date(self.sale_date.year, self.sale_date.month,
-                1)
+            interval = relativedelta(months=1, days=-1)
+            start = datetime.date(date.year, date.month, 1)
         elif period == 'biweekly':
-            interval_date = relativedelta(weeks=2)
-            min_date = datetime.date(self.sale_date.year, self.sale_date.month,
-                1 if self.sale_date.day <= 15 else 15)
+            if date.day <= 15:
+                start_day = 1
+                interval = relativedelta(day=15)
+            else:
+                start_day = 16
+                interval = relativedelta(months=1, day=1, days=-1)
+            start = datetime.date(date.year, date.month, start_day)
+        elif period == 'ten-days':
+            if date.day <= 10:
+                start_day = 1
+                interval = relativedelta(day=10)
+            elif date.day <= 20:
+                start_day = 11
+                interval = relativedelta(day=20)
+            else:
+                start_day = 21
+                interval = relativedelta(months=1, day=1, days=-1)
+            start = datetime.date(date.year, date.month, start_day)
+        elif period.startswith('weekly'):
+            diff = date.weekday() - int(period[-1])
+            if diff < 0:
+                diff = 7 + diff
+            start = date - relativedelta(days=diff)
+            interval = relativedelta(days=6)
         elif period == 'daily':
-            interval_date = relativedelta(day=0)
-            min_date = datetime.date.today()
-
-        return max(min_date, (self.sale_date - interval_date))
-
-    def _get_maximum_invoice_date(self):
-        period = self.party.sale_invoice_grouping_period
-        if not period:
-            return
-        _, last_day = calendar.monthrange(self.sale_date.year,
-            self.sale_date.month)
-        if period == 'monthly':
-            interval_date = relativedelta(months=1)
-            max_date = datetime.date(self.sale_date.year, self.sale_date.month,
-                last_day)
-        elif period == 'biweekly':
-            interval_date = relativedelta(weeks=2)
-            max_date = datetime.date(self.sale_date.year, self.sale_date.month,
-                15 if self.sale_date.day <= 15 else last_day)
-        elif period == 'daily':
-            interval_date = relativedelta(day=0)
-            max_date = datetime.date.today()
-
-        return min(max_date, (self.sale_date + interval_date))
+            start = datetime.date.today()
+            interval = relativedelta(day=0)
+        return start, start + interval
 
     def _get_invoice_sale(self):
         Lang = Pool().get('ir.lang')
@@ -78,17 +85,20 @@ class Sale:
         invoice = super(Sale, self)._get_invoice_sale()
 
         period = self.party.sale_invoice_grouping_period
-        if period:
-            for code in [Transaction().language, 'en']:
+        if self.invoice_grouping_method == 'standard' and period:
+            for code in [Transaction().language, 'en_US']:
                 langs = Lang.search([
                         ('code', '=', code),
-                        ])
+                        ], limit=1)
                 if langs:
                     break
             lang, = langs
-            start_date = Lang.strftime(self._get_minimum_invoice_date(),
-                lang.code, lang.date)
-            end_date = Lang.strftime(self._get_maximum_invoice_date(),
-                lang.code, lang.date)
-            invoice.description = "%s - %s" % (start_date, end_date)
+            date = self._get_grouped_invoice_date()
+            start, end = self._get_invoice_dates(date,
+                self.party.sale_invoice_grouping_period)
+            invoice.start_date = start
+            invoice.end_date = end
+            start, end = [Lang.strftime(x, lang.code, lang.date) for x in
+                (start, end)]
+            invoice.description = '%s - %s' % (start, end)
         return invoice
